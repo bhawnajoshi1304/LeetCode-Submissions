fb103b9f40c7358434c19a1e5e81453edc7fe1fd
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2022-04-28 22:05:53 +0530

    Time: 18 ms, Memory: 10.6 MB - LeetHub

---
+++ third-maximum-number/third-maximum-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        set<long> s;
+        for(auto i:nums){
+            s.insert((long)-1*i);
+        }
+        long j=0,ans=*s.begin();
+        for(auto i:s){
+            if(j==3) break;
+            j+=1;
+            ans=i;
+            cout<<i<<ans<<"\n";
+        }
+        if(j!=3) return (-1)*(*s.begin());
+        return ans*(-1);
+    }
+};
