d47a2ce13e543784320f23c64825f97f77c7d59b
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2022-07-27 03:18:10 +0530

    Time: 111 ms (48.48%), Space: 48.2 MB (93.28%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -9,15 +9,9 @@ public:
         return b;
     }
     bool canJump(vector<int>& nums) {
-        vector<bool> can(nums.size(),false);
-        can[0]=true;
-        int tmp=0;
-        for(int i=0;i<nums.size();i+=1){
-            for(int j=max(tmp,i+1);j<min(nums.size(),i+nums[i]+1);j+=1){
-                tmp=j;
-                can[j]=can[i];
-        }
-        }
-        return can[nums.size()-1];
+        int idx = 0;
+        for (int i = 0; idx < nums.size() && idx <= i; ++idx)
+            i = max(idx + nums[idx], i);
+        return idx == nums.size();
     }
 };
