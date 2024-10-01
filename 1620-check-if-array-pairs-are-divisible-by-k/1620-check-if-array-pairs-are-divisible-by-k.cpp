26777f3221919c709e04d9b0ff1954f62dfae11c
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2024-10-01 06:38:08 +0530

    Time: 230 ms (7.01%), Space: 82.6 MB (5.42%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -10,12 +10,10 @@ public:
             m[k+i%k]+=1;
             else
             m[i % k] += 1;
-            cout<<i%k<<"\n";
         }
         if (m[0] % 2 != 0)
             return false;
         for (int i = 1; i < (k-1) / 2 +1; i += 1) {
-            cout<<i<<" "<<k-i<<" "<<m[i]<<" "<<m[k-i]<<"\n";
             if (m[i] != m[k - i])
                 return false;
         }
