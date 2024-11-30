f068699ab4790c11d05f4d9d439f5707ec8195d4
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2024-11-30 15:08:35 +0530

    Time: 2 ms (40.63%), Space: 10.5 MB (23.28%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    void generatePermutations(vector<int> &nums,vector<int> &tmp, vector<bool> &flag, vector<vector<int>> &ans){
-        if(tmp.size() == nums.size()) {ans.push_back(tmp);return;}
-        for(int i=0;i<nums.size();i+=1){
-            if(!flag[i]){
-                tmp.push_back(nums[i]);
-                flag[i] = true;
-                generatePermutations(nums,tmp,flag,ans);
-                tmp.pop_back();
-                flag[i]=false;
-            }
+    void generatePermutations(vector<int>& nums, vector<vector<int>>& ans, int ind = 0) {
+        if (ind == nums.size() - 1)
+            ans.push_back(nums);
+        for (int i = ind; i < nums.size(); i += 1) {
+            swap(nums[i], nums[ind]);
+            generatePermutations(nums, ans, ind+1);
+            swap(nums[i], nums[ind]);
         }
-
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> tmp;
-        vector<bool> flag(nums.size(),false);
-        generatePermutations(nums,tmp,flag,ans);
+        generatePermutations(nums, ans);
         return ans;
     }
 };
