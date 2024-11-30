a2457dd84575641618a5a2999d5e44340eff7baa
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2024-11-30 14:06:15 +0530

    Time: 0 ms (100%), Space: 10.2 MB (30.81%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    void powerset(vector<vector<int>> &ans, vector<int> a,int x){
-        if(x<a.size()){
-            int N=ans.size();
-            for(int i=0;i<N;i+=1){
-                vector<int> tmp;
-                for(int j=0;j<ans[i].size();j+=1)
-                    tmp.push_back(ans[i][j]);
-                tmp.push_back(a[x]);
-                sort(tmp.begin(),tmp.end());
-                ans.push_back(tmp);
+    void subsets(vector<int> &nums, vector<int> &tmp, vector<vector<int>> &ans, int i=0){
+        ans.push_back(tmp);
+        for(int j=i;j<nums.size();j+=1){
+            if(j==i || nums[j]!=nums[j-1]){
+                tmp.push_back(nums[j]);
+                subsets(nums,tmp,ans,j+1);
+                tmp.pop_back();
             }
-            powerset(ans,a,x+1);
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans(1);
-        powerset(ans,nums,0);
-        set<vector<int>> s;
-        for(auto i:ans)
-            s.insert(i);
-        vector<vector<int>> a;
-        for(auto i:s)
-            a.push_back(i);
-        return a;
+        vector<vector<int>> ans;
+        vector<int> tmp;
+        sort(nums.begin(),nums.end());
+        subsets(nums,tmp,ans);
+        return ans;
     }
 };
