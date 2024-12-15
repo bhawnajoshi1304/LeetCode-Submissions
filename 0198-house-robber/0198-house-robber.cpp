3e9413a1b5dd8c554f162eb7df9784ab19480509
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2024-12-16 01:29:33 +0530

    Time: 0 ms (100%), Space: 10.1 MB (25.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         if(nums.size() == 1) return nums[0];
-        vector<int> dp(nums.size());
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
+        int prev2 = nums[0];
+        int prev1 = max(nums[0],nums[1]);
+        int curr = prev1;
         for(int i=2;i<nums.size();i+=1){
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+            curr = max(nums[i]+prev2,prev1);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[nums.size()-1];
+        return curr;
     }
 };
