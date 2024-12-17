4115164d0fb3178399e4f8d97bbc60114201d5c2
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2024-12-17 23:02:02 +0530

    Time: 0 ms (100%), Space: 14.3 MB (35.63%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int m=matrix.size(),n=matrix[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,INT_MAX));
-        for(int i=0;i<m;i+=1){
-            for(int j=0;j<n;j+=1){
-                if(i==0)
-                dp[i][j]=matrix[i][j];
-                if(i>0 && j>0)
-                dp[i][j]=min(dp[i-1][j-1]+matrix[i][j],dp[i][j]);
-                if(i>0)
-                dp[i][j]=min(dp[i-1][j]+matrix[i][j],dp[i][j]);
-                if(i>0 && j+1<n)
-                dp[i][j]=min(dp[i-1][j+1]+matrix[i][j],dp[i][j]); 
-                cout<<dp[i][j]<<" "; 
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> prev(n, INT_MAX), curr(n, INT_MAX);
+        for (int i = 0; i < m; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                if (i == 0)
+                    curr[j] = prev[j] = matrix[i][j];
+                else {
+                    curr[j] = prev[j] + matrix[i][j];
+                    if (i > 0 && j > 0)
+                        curr[j] = min(prev[j - 1] + matrix[i][j], curr[j]);
+                    if (i > 0 && j + 1 < n)
+                        curr[j] = min(prev[j + 1] + matrix[i][j], curr[j]);
+                }
             }
-            cout<<"\n";
-
+            for (int j = 0; j < n; j += 1)
+                prev[j]=curr[j];
         }
-        int ans=INT_MAX;
-        for(int j=0;j<n;j+=1){
-            ans=min(ans,dp[m-1][j]);
+        int ans = INT_MAX;
+        for (int j = 0; j < n; j += 1) {
+            ans = min(ans, curr[j]);
         }
         return ans;
     }
