c1159ba39477555090184a2105c5a8b3529803ed
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2024-12-27 19:32:19 +0530

    Time: 0 ms (100%), Space: 12.3 MB (62.58%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -6,22 +6,31 @@ public:
         // sum1+sum2 = sum
         // sum1 = (target+sum)/2
         // number of ways to have sum sum1
-        int sum1,sum=0,n=nums.size();
-        for(auto i: nums){
-            sum+=i;
+        int sum1, sum = 0, n = nums.size();
+        for (auto i : nums) {
+            sum += i;
         }
-        sum1 = (target+sum)/2;
-        if((target+sum) != 2*sum1 || sum1<0) return 0;
-        vector<vector<int>> dp(n+1,vector<int>(sum1+1,0));
-        dp[0][0] = 1;
-        for(int i=1;i<=n;i+=1){
-            for(int j=0;j<=sum1;j+=1){
-                if(j>=nums[i-1])
-                dp[i][j] = dp[i-1][j]+dp[i-1][j-nums[i-1]];
-                else
-                dp[i][j] = dp[i-1][j];
+        sum1 = (target + sum) / 2;
+        if ((target + sum) != 2 * sum1 || sum1 < 0)
+            return 0;
+        // vector<vector<int>> dp(n + 1, vector<int>(sum1 + 1, 0));
+        // dp[0][0] = 1;
+        // for (int i = 1; i <= n; i += 1) {
+        //     for (int j = 0; j <= sum1; j += 1) {
+        //         if (j >= nums[i - 1])
+        //             dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
+        //         else
+        //             dp[i][j] = dp[i - 1][j];
+        //     }
+        // }
+        // return dp[n][sum1];
+        vector<int> dp(sum1 + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i += 1) {
+            for (int j = sum1; j >= nums[i - 1]; j -= 1) {
+                dp[j] = dp[j] + dp[j - nums[i - 1]];
             }
         }
-    return dp[n][sum1];
+        return dp[sum1];
     }
 };
