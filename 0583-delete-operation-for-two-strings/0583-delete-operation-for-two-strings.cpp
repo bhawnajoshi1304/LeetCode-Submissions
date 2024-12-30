ff9dceeddbd8f6aa138ef73ed65851b86c20d4e5
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2024-12-31 05:19:42 +0530

    Time: 3 ms (97.86%), Space: 11.2 MB (93.63%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -5,16 +5,29 @@ public:
         // String2:   ea t
         // LCS:       ea
         int n=word1.size(),m=word2.size();
-        vector<vector<int>> dp(n,vector<int>(m,0));
+        // vector<vector<int>> dp(n,vector<int>(m,0));
+        // for(int i=0;i<n;i+=1){
+        //     for(int j=0;j<m;j+=1){
+        //         if(word1[i]==word2[j])
+        //         dp[i][j]=(i==0 || j==0)? 1: dp[i-1][j-1]+1;
+        //         else
+        //         dp[i][j]=max(i>0?dp[i-1][j]:0,j>0?dp[i][j-1]:0);
+                    
+        //     }
+        // }
+        // return m+n-2*dp[n-1][m-1];
+        vector<int> dp(m,0);
+        int prevRow,prevRowCol;
         for(int i=0;i<n;i+=1){
             for(int j=0;j<m;j+=1){
+                prevRow=dp[j];
                 if(word1[i]==word2[j])
-                dp[i][j]=(i==0 || j==0)? 1: dp[i-1][j-1]+1;
+                dp[j]=(i==0 || j==0)? 1: prevRowCol+1;
                 else
-                dp[i][j]=max(i>0?dp[i-1][j]:0,j>0?dp[i][j-1]:0);
-                    
+                dp[j]=max(i>0?prevRow:0,j>0?dp[j-1]:0);
+                prevRowCol=prevRow;
             }
         }
-        return m+n-2*dp[n-1][m-1];
+        return m+n-2*dp[m-1];
     }
 };
