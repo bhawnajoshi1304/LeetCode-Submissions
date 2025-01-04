9f5b86f3190800d9192eac5cc936734c519102f4
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2025-01-05 03:20:19 +0530

    Time: 131 ms (89.78%), Space: 15.9 MB (69.04%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        vector<int> a(26,-1),triplets(26,0);
-        int ans=0,n=s.size();
-        for(int i=0;i<n;i+=1){
-            if(a[s[i]-'a']!=-1){
-                ans+=i-a[s[i]-'a']-1;
-                a[s[i]-'a']=i;
-                triplets[s[i]-'a']+=1;
-                if(triplets[s[i]-'a']==3)
-                ans+=1;
-            }else{
-                a[s[i]-'a']=i;
-                triplets[s[i]-'a']+=1;
+        vector<int> first = vector(26, -1);
+        vector<int> last = vector(26, -1);
+        
+        for (int i = 0; i < s.size(); i++) {
+            int curr = s[i] - 'a';
+            if (first[curr] == - 1) {
+                first[curr] = i;
             }
+            
+            last[curr] = i;
         }
+        
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            if (first[i] == -1) {
+                continue;
+            }
+            
+            unordered_set<char> between;
+            for (int j = first[i] + 1; j < last[i]; j++) {
+                between.insert(s[j]);
+            }
+            
+            ans += between.size();
+        }
+        
         return ans;
     }
 };
