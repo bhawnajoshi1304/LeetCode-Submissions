9f840c19566565972145806c19538b138f611a98
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2025-01-14 22:00:43 +0530

    Time: 0 ms (100%), Space: 19.3 MB (76.35%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,29 +1,23 @@
 class Solution {
-public:
-void dfs(int x, vector<vector<int>> &adj, vector<bool> &visited){
-    visited[x]=true;
-    cout<<x<<" ";
-    for(auto i: adj[x]){
-        if(!visited[i])
-            dfs(i,adj,visited);
-    }
-}
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<vector<int>> adj(n);
-        for(int i=0;i<n;i+=1){
-            for(int j=0;j<n;j+=1){
-                if(i!=j && isConnected[i][j])
-                adj[i].push_back(j);
+private:
+    void dfs(vector<bool>& visited, vector<vector<int>>& isConnected, int x) {
+        for (int i = 0; i < isConnected[x].size(); i++) {
+            if (isConnected[x][i] == 1 && !visited[i]) {
+                visited[i] = true;
+                dfs(visited, isConnected, i);
             }
         }
+    }
+
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int ans = 0, n = isConnected.size();
         vector<bool> visited(n,false);
-        int ans=0;
-        for(int i=0;i<n;i+=1){
-            if(!visited[i]){
-                ans+=1;
-                dfs(i,adj,visited);
-                cout<<"\n";
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(visited, isConnected, i);
+                ans++;
             }
         }
         return ans;
