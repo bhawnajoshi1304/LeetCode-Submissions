b10045acf403444be3c5902b12fd8d8d835df61a
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2025-03-16 22:24:55 +0530

    Time: 0 ms (100%), Space: 9.4 MB (50.21%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool valid(int i, int j, int n, int m, vector<vector<int>>& matrix) {
+        return (i < n && j < m && i >= 0 && j >= 0 && matrix[i][j] != INT_MIN);
+    }
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans, y = {1, 0, -1, 0}, x = {0, 1, 0, -1};
+        int d = 0;
+        int n = matrix.size(), m = matrix[0].size(), i = 0, j = 0;
+        while (ans.size() != n * m) {
+            cout << i << " " << j << "\n";
+            ans.push_back(matrix[i][j]);
+            matrix[i][j] = INT_MIN;
+            if (!valid(i + x[d], j + y[d], n, m, matrix)) {
+                d = (d + 1) % 4;
+                cout << d << "\n";
+            }
+            i += x[d];
+            j += y[d];
+        }
+        return ans;
+    }
+};
