5001640128aba7f3d93c2f81f9571afce655a18e
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2025-08-04 04:31:34 +0530

    Time: 51 ms (48.62%), Space: 28.3 MB (56.27%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
+    int n;
+    vector<vector<vector<int>>> dp;
+
+    int dfs(vector<vector<int>>& grid, int r1, int c1, int c2) {
+        int r2 = r1 + c1 - c2;
+
+        if (r1 >= n || c1 >= n || r2 >= n || c2 >= n ||
+            grid[r1][c1] == -1 || grid[r2][c2] == -1)
+            return -1e9;
+
+        if (r1 == n - 1 && c1 == n - 1)
+            return grid[r1][c1];
+
+        if (dp[r1][c1][c2] != -1)
+            return dp[r1][c1][c2];
+
+        int cherries = grid[r1][c1];
+        if (r1 != r2 || c1 != c2)
+            cherries += grid[r2][c2];
+
+        int best = max({
+            dfs(grid, r1 + 1, c1, c2 + 1),
+            dfs(grid, r1, c1 + 1, c2 + 1),
+            dfs(grid, r1 + 1, c1, c2),
+            dfs(grid, r1, c1 + 1, c2)
+        });
+
+        return dp[r1][c1][c2] = cherries + best;
+    }
+
     int cherryPickup(vector<vector<int>>& grid) {
-        int n = grid.size();
-
-        vector<vector<vector<int>>> dp(2*n-1, vector<vector<int>>(n, vector<int>(n, -1)));
-        dp[0][0][0] = grid[0][0];
-
-        for (int k = 1; k < 2 * n - 1; ++k) {
-            for (int x1 = max(0, k - n + 1); x1 <= min(n - 1, k); ++x1) {
-                for (int x2 = max(0, k - n + 1); x2 <= min(n - 1, k); ++x2) {
-                    int y1 = k - x1, y2 = k - x2;
-                    if (y1 >= n || y2 >= n || grid[x1][y1] == -1 || grid[x2][y2] == -1)
-                        continue;
-
-                    int cherries = grid[x1][y1];
-                    if (x1 != x2 || y1 != y2)
-                        cherries += grid[x2][y2];
-
-                    int best = -1;
-
-                    for (int dx1 : {-1, 0}) {
-                        for (int dx2 : {-1, 0}) {
-                            int px1 = x1 + dx1, px2 = x2 + dx2;
-                            int py1 = k - 1 - px1, py2 = k - 1 - px2;
-
-                            if (px1 >= 0 && px2 >= 0 && py1 >= 0 && py1 < n && py2 >= 0 && py2 < n && dp[k - 1][px1][px2] != -1) {
-                                best = max(best, dp[k - 1][px1][px2]);
-                            }
-                        }
-                    }
-
-                    if (best != -1)
-                        dp[k][x1][x2] = best + cherries;
-                }
-            }
-        }
-
-        return max(0, dp[2 * n - 2][n - 1][n - 1]);
+        n = grid.size();
+        dp = vector<vector<vector<int>>>(n, vector<vector<int>>(n, vector<int>(n, -1)));
+        return max(0, dfs(grid, 0, 0, 0));
     }
 };
