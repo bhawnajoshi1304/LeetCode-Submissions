2f7d0a0ccbe235c2995b648c7c433885499fceaa
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2026-01-03 02:34:11 +0530

    Time: 1 ms (49.81%), Space: 28.5 MB (69.81%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        set<int> s;
-        for(auto i:nums){
-            if(s.find(i)==s.end()){
-                s.insert(i);
-            }else return i;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(i+1< n && nums[i]==nums[i+1]){
+                return nums[i];
+            }
+            if(i+2<n && nums[i]==nums[i+2]){
+                return nums[i];
+            }
         }
-        return nums[0];
+        return nums.back();
     }
 };
