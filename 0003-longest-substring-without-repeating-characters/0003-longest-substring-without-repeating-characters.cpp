ae6869e6fbf82366df28bc3d76048a08ef86358c
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2026-04-23 05:03:57 +0530

    Time: 7 ms (64.58%), Space: 12.1 MB (44.02%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,22 +4,17 @@ public:
         int ans = 0, curr = -1;
         map<char, int> m;
         for (int i = 0; i < s.size(); i += 1) {
-            cout << s[i] << "\n";
             if (m.find(s[i]) == m.end()) {
                 m[s[i]] = i;
             } else {
                 if (curr == -1) {
                     ans = max(ans, i);
-                    cout << i << " " << ans << "\n";
                 } else {
                     ans = max(ans, i - curr - 1);
-                    cout << i << " " << ans << "\n";
                 }
-                ans = max(i - max(curr,m[s[i]]), ans);
-                cout << i << " " << m[s[i]] << " " << ans << "\n";
-                curr = max(curr,m[s[i]]);
+                curr = max(curr, m[s[i]]);
+                ans = max(i - curr, ans);
                 m[s[i]] = i;
-                cout << "*" << curr << " " << m[s[i]] << " " << ans << "\n";
             }
         }
         ans = max((int)s.size() - 1 - curr, ans);
