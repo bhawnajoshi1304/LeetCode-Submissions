53ceb96efe24f034d45609afeae7c54d9accade4
BHAWNA JOSHI <bhawnajoshi1304@gmail.com>
2026-09-13 12:37:46 +0530

    Time: 17 ms (98.01%), Space: 45.9 MB (7.76%) - LeetHub

---
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.java
@@ -0,0 +1,30 @@
+class FooBar {
+    private int n;
+    private Semaphore foo,bar;
+
+    public FooBar(int n) {
+        this.n = n;
+        this.foo=new Semaphore(1);
+        this.bar = new Semaphore(0);
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            foo.acquire();
+        	// printFoo.run() outputs "foo". Do not change or remove this line.
+        	printFoo.run();
+            bar.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            bar.acquire();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+        	printBar.run();
+            foo.release();
+        }
+    }
+}
